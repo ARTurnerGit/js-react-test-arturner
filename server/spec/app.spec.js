92cb51e689ad4306b3ad872432fed0e9148f6b2e
Alun Turner <alun.turner@googlemail.com>
2020-09-16 10:14:01 +0100

    creating /spec files for testing

---
+++ server/spec/app.spec.js
@@ -0,0 +1,16 @@
+const chai = require("chai");
+const { expect } = chai;
+const request = require("supertest");
+const app = require("../src/server");
+const { db } = require("../db/dbConfig");
+
+describe("APP", () => {
+  it("404: msg 'path not found' if non-existant path is used", () => {
+    return request(app)
+      .get("/not_a_path")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).to.equal("path not found");
+      });
+  });
+});
