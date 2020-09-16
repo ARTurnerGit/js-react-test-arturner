3ea616361dfdef671cb3192b4b466d8bb8233ac8
Alun Turner <alun.turner@googlemail.com>
2020-09-16 11:34:49 +0100

    converting LabelledInput into controlled component for all inputs

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import LabelledInput, {
   LabelledInputTextType,
   LabelledInputSubmitType,
@@ -6,6 +6,13 @@ import LabelledInput, {
 } from "../../components/LabelledInput";
 
 function Form({ children }) {
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [message_text, setMessage_text] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
   };
@@ -20,12 +27,42 @@ function Form({ children }) {
         justifyContent: "space-evenly",
       }}
     >
-      <LabelledInput label={"First Name*"} type={LabelledInputTextType} />
-      <LabelledInput label={"Last Name*"} type={LabelledInputTextType} />
-      <LabelledInput label={"E-mail*"} type={LabelledInputEmailType} />
-      <LabelledInput label={"Phone number*"} type={LabelledInputTextType} />
-      <LabelledInput label={"Address"} type={LabelledInputTextType} />
-      <LabelledInput label={"Message*"} type={LabelledInputTextType} />
+      <LabelledInput
+        label={"First Name*"}
+        type={LabelledInputTextType}
+        controlValue={first_name}
+        setControlValue={setFirst_name}
+      />
+      <LabelledInput
+        label={"Last Name*"}
+        type={LabelledInputTextType}
+        controlValue={last_name}
+        setControlValue={setLast_name}
+      />
+      <LabelledInput
+        label={"E-mail*"}
+        type={LabelledInputEmailType}
+        controlValue={email}
+        setControlValue={setEmail}
+      />
+      <LabelledInput
+        label={"Phone number*"}
+        type={LabelledInputTextType}
+        controlValue={phone}
+        setControlValue={setPhone}
+      />
+      <LabelledInput
+        label={"Address"}
+        type={LabelledInputTextType}
+        controlValue={address}
+        setControlValue={setAddress}
+      />
+      <LabelledInput
+        label={"Message*"}
+        type={LabelledInputTextType}
+        controlValue={message_text}
+        setControlValue={setMessage_text}
+      />
       <LabelledInput type={LabelledInputSubmitType} />
     </form>
   );
--- client/src/components/LabelledInput/index.js
+++ client/src/components/LabelledInput/index.js
@@ -4,11 +4,28 @@ export const LabelledInputTextType = "text";
 export const LabelledInputSubmitType = "submit";
 export const LabelledInputEmailType = "email";
 
-function LabelledInput({ type = LabelledInputTextType, label = false }) {
+function LabelledInput({
+  type = LabelledInputTextType,
+  label = false,
+  controlValue,
+  setControlValue,
+}) {
+  if (type === "submit") {
+    return (
+      <>
+        <input id={type} type={type} />
+      </>
+    );
+  }
   return (
     <>
-      {label && <label htmlFor="">{label}</label>}
-      <input type={type} />
+      {label && <label htmlFor={label}>{label}</label>}
+      <input
+        id={label}
+        type={type}
+        value={controlValue}
+        onChange={(e) => setControlValue(e.target.value)}
+      />
     </>
   );
 }
