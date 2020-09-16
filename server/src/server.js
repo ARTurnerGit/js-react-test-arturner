934b2f639e09cfc699cbd5d1543ba4d860364950
Alun Turner <alun.turner@googlemail.com>
2020-09-16 10:49:18 +0100

    neatening up server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,50 +1,31 @@
+// dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-// const mysql = require("mysql");
-const { db } = require("../db/dbConfig");
 const cors = require("cors");
+const { db } = require("../db/dbConfig");
+
+// routers
 const { testRouter } = require("../routers/test");
 const { createRouter } = require("../routers/create");
 const { invalidPathRouter } = require("../errors");
 
 const app = express();
 
-//STATIC FOLDER
+// static folder, body parser, cors
 app.use(express.static(path.join(__dirname, "../client/build")));
-
-// Body Parser Middleware
 app.use(bodyParser.json());
-
-// Deal with CORS
 app.use(cors());
 
-//CONNECT
-db.connect((err) => {
-  if (err) throw err;
-  // console.log("MySQL Connected...");
-});
-
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
+// //CONNECT
+// db.connect((err) => {
+//   if (err) throw err;
+//   // console.log("MySQL Connected...");
 // });
 
-// Test
+// routes
 app.use("/", testRouter);
-
-// Create
 app.use("/create", createRouter);
-
-// An API endpoint to store form post data
-app.post("/create", (req, res) => {
-  return res.json({
-    status: res.statusCode,
-    data: {
-      message: "Success",
-    },
-  });
-});
-
 app.use("/*", invalidPathRouter);
 
 module.exports = app;
