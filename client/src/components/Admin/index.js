78460e9124af4c729054acb2df237806db9aa623
Alun Turner <alun.turner@googlemail.com>
2020-09-16 13:01:07 +0100

    FINAL COMMIT ADDING /ADMIN TO FRONT END

--- client/src/components/Admin/index.js
+++ client/src/components/Admin/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Admin(props) {
-  return <p>this is the admin component</p>;
+  const [data, setData] = useState({ results: [], fetched: false });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("http://localhost:8080/admin").then((res) =>
+        res.json()
+      );
+      setData({
+        results: data.results,
+        fetched: true,
+      });
+    };
+
+    if (!data.fetched) {
+      fetchData();
+    }
+  });
+  return (
+    <>
+      {data.results.map((msgObj) => {
+        return (
+          <p key={msgObj.message_id}>
+            FROM:{msgObj.message_first_name} {msgObj.message_last_name} <br />
+            AT:{msgObj.message_created_at} <br />
+            MSG: {msgObj.message_text}
+          </p>
+        );
+      })}
+    </>
+  );
 }
 
 export default Admin;
