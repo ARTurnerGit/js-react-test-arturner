b5a9b2d84baf1dd964db68d7cfb9cccf74e45b03
Alun Turner <alun.turner@googlemail.com>
2020-09-24 11:15:06 +0100

    added axios mocking and post test for form

--- client/src/components/Form/Form.test.js
+++ client/src/components/Form/Form.test.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import axios from "axios";
+import { render, fireEvent, act, wait } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Form from "./index";
+
+jest.mock("axios");
 
 describe("RENDER", () => {
   test("renders input boxes on /home", () => {
@@ -15,5 +18,57 @@ describe("RENDER", () => {
     getByLabelText("Message*");
 
     getByTestId("submit");
+  });
+});
+
+describe("POST", () => {
+  test("for valid data, will submit all data required to /create", async () => {
+    axios.post.mockImplementationOnce(() =>
+      Promise.resolve({ status: 201, data: { msg: "quick test" } })
+    );
+
+    const { getByLabelText, getByTestId, getByText } = render(<Form />);
+
+    // input into the text fields
+    act(() => {
+      fireEvent.change(getByLabelText("First Name*"), {
+        target: { value: "first" },
+      });
+      fireEvent.change(getByLabelText("Last Name*"), {
+        target: { value: "last" },
+      });
+      fireEvent.change(getByLabelText("E-mail*"), {
+        target: { value: "a@b.com" },
+      });
+      fireEvent.change(getByLabelText("Phone number*"), {
+        target: { value: "1234" },
+      });
+      fireEvent.change(getByLabelText("Address"), { target: { value: "" } });
+      fireEvent.change(getByLabelText("Message*"), {
+        target: { value: "My axios post test" },
+      });
+    });
+
+    // click submit
+    act(() => {
+      fireEvent.click(getByTestId("submit"));
+    });
+
+    // check it's called once, that it's calling the correct path and data, and that the return message is rendered
+    expect(axios.post).toHaveBeenCalledTimes(1);
+    expect(axios.post).toHaveBeenCalledWith(
+      "http://localhost:8080/create",
+      expect.objectContaining({
+        first_name: "first",
+        last_name: "last",
+        email: "a@b.com",
+        phone: "1234",
+        address: "",
+        message_text: "My axios post test",
+      })
+    );
+    await wait(() => {
+      getByText("quick test");
+    });
   });
 });
